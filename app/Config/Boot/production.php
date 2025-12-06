01fb290e4e3f07e66eea90bcb1a31805785d916e
FadhliRajwaa <fadhlirajwaarahmana@gmail.com>
2025-12-06 22:03:02 +0700

    Enable debug mode in production temporarily

--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -12,14 +12,13 @@
 error_reporting(E_ALL & ~E_DEPRECATED);
 // If you want to suppress more types of errors.
 // error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-ini_set('display_errors', '0');
 
-/*
- |--------------------------------------------------------------------------
- | DEBUG MODE
- |--------------------------------------------------------------------------
- | Debug mode is an experimental flag that can allow changes throughout
- | the system. It's not widely used currently, and may not survive
- | release of the framework.
- */
-defined('CI_DEBUG') || define('CI_DEBUG', false);
+    /*
+     * --------------------------------------------------------------------------
+     * Display Error (Debug Mode)
+     * --------------------------------------------------------------------------
+     *
+     * If an error occurs, we want to see the details to debug it.
+     */
+    ini_set('display_errors', '1');
+    defined('CI_DEBUG') || define('CI_DEBUG', true);
